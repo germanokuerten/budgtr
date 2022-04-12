f04045651a3f80cf2437582afd4ac893b57c80f0
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-11 20:30:28 -0700

    project setup

---
+++ server.js
@@ -0,0 +1,55 @@
+//////////////////////
+// Budgtr app
+//////////////////////
+
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT
+
+const budget = require("./models/budget.js")
+
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+
+
+//////////////
+// Middleware
+//////////////
+
+app.use(express.urlencoded({extended: false}))
+app.use(morgan("tiny"))
+app.use("/static", express.static("public"))
+app.use(methodOverride("_method")) 
+
+
+//////////////
+// Routes
+//////////////
+
+app.get("/", (req, res) => {
+    res.send("Home")
+})
+
+// Index - GET /budgets
+
+
+
+
+// Show - GET /budgets/:index
+
+// New - GET /budgets/new
+
+// Create -POST /budgets
+
+
+
+
+//////////////
+// Listener
+//////////////
+
+app.listen(PORT, () => {
+    console.log(`I am listening in ${PORT}`)
+})
